6d76c9025db095020d3b8a5fd7bba51c08f36e6a
theoschweizer <schweizer.theodore@gmail.com>
2024-01-13 15:13:47 -0600

    teleportation

--- Main.gd
+++ Main.gd
@@ -5,6 +5,9 @@ extends Node2D
 func _on_quit_pressed():
 	get_tree().quit()
 
-
-func _on_play_pressed():
+func _on_tutorial_pressed():
 	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
