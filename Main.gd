616b477523196d5c566857a9616e72215f67a210
theoschweizer <schweizer.theodore@gmail.com>
2024-01-16 22:41:53 -0600

    done with doors

--- Main.gd
+++ Main.gd
@@ -9,5 +9,5 @@ func _on_tutorial_pressed():
 	get_tree().change_scene_to_file("res://levels/tutorial.tscn")
 
 func _on_level_1_pressed():
-	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+	get_tree().change_scene_to_file("res://levels/escher.tscn")
 
