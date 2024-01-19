fbc74405452afd2e207305ba630e786b03a68657
Moonbear2001 <desmondr1@aol.com>
2024-01-18 21:41:04 -0600

    finished lever, added animations

--- objects/lever.gd
+++ objects/lever.gd
@@ -7,15 +7,34 @@ Lever
 signal toggle_moving_platform
 
 @onready var interaction_area = $InteractionArea
-@onready var rng = RandomNumberGenerator.new()
+@onready var animation_player = $Sprite2D/AnimationPlayer
 
+# Possible lever states
+enum states {
+	ON,
+	OFF,
+	NEUTRAL,
+}
+
+# Current state
+var state
+
+# Ready
 func _ready():
 	interaction_area.interact = Callable(self, "pull_lever")
+	state = states.NEUTRAL
+	animation_player.play("neutral")
 
-# Change color when interacted with
+# Interaction with the lever
 func pull_lever():
 	
-	# TODO: Add animation
+	# Control state and animation
+	if state == states.NEUTRAL or state == states.OFF:
+		state = states.ON
+		animation_player.play("on")
+	else:
+		state = states.OFF
+		animation_player.play("off")
 	
 	# Toggle platform movement
 	toggle_moving_platform.emit()
