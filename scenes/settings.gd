eb964f1eea4213d8a1367a53f27915731978e48f
dcr4 <dcr4@rice.edu>
2024-02-11 22:01:38 -0600

    more audio dev
    added volume control system to menu, added spatial audio in menu

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -19,3 +19,21 @@ func _on_quit_btn_pressed():
 
 func _on_menu_btn_pressed():
 	SceneManager.load_new_scene("res://levels/menu.tscn")
+	
+	
+"""
+Volume control.
+Two sliders to adjust the volume, completely mutes the bus below a certain 
+threshold
+"""
+	
+@onready var SFX_BUS_ID = AudioServer.get_bus_index("SFX")
+@onready var MUSIC_BUS_ID = AudioServer.get_bus_index("Music")
+
+func _on_music_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(MUSIC_BUS_ID, linear_to_db(value))
+	AudioServer.set_bus_mute(MUSIC_BUS_ID, value < 0.05)
+
+func _on_sfx_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(SFX_BUS_ID, linear_to_db(value))
+	AudioServer.set_bus_mute(SFX_BUS_ID, value < 0.05)
