530c784126d7aff61898cabd11386bd66171d6fb
theoschweizer <schweizer.theodore@gmail.com>
2024-02-15 15:21:07 -0600

    settings

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -6,7 +6,8 @@ Window for navigating to the menu, controlling volume, and resetting high scores
 
 @onready var SFX_BUS_ID = AudioServer.get_bus_index("SFX")
 @onready var MUSIC_BUS_ID = AudioServer.get_bus_index("Music")
-
+@onready var sfx_check = $CanvasLayer/SfxCheckBtn
+@onready var node = $CanvasLayer/Node2D
 @onready var canvas_layer = $CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
@@ -19,7 +20,7 @@ func toggle_settings_window() -> void:
 		canvas_layer.hide()
 	else:
 		canvas_layer.show()
-		
+			
 # Hide the settings sindow
 func hide_settings_window() -> void:
 	canvas_layer.hide()
@@ -47,17 +48,24 @@ func _on_reset_scores_btn_pressed():
 # Toggle music on/off
 func _on_music_check_btn_toggled(toggled_on):
 	if toggled_on:
-		print("on")
 		#AudioServer.set_bus_volume_db(MUSIC_BUS_ID, 0)
 		AudioServer.set_bus_mute(MUSIC_BUS_ID, false)
 	else:
-		print("off")
 		AudioServer.set_bus_mute(MUSIC_BUS_ID, true)
 
 # Toggle sfx on/off
 func _on_sfx_check_btn_toggled(toggled_on):
 	if toggled_on:
-		
+		sfx_check.modulate = Color(239, 232, 223)
 		AudioServer.set_bus_volume_db(SFX_BUS_ID, 0)
 	else:
 		AudioServer.set_bus_mute(SFX_BUS_ID, true)
+
+
+func _on_restart_btn_pressed():
+	get_tree().reload_current_scene()
+	canvas_layer.hide()
+
+func _on_close_btn_pressed():
+	canvas_layer.hide()
+
