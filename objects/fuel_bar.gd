2da0ac45f4e1419a0e4ba8faf4e99b47f213fe77
dcr4 <dcr4@rice.edu>
2024-02-21 01:36:11 -0600

    Prototype Darkness/Lantern/Refill Station mechanic

---
+++ objects/fuel_bar.gd
@@ -0,0 +1,13 @@
+extends Control
+
+"""
+Fuel bar that appears in the UI indicating how much fuel for the lantern is left.
+"""
+
+@onready var progress_bar = $ProgressBar
+
+func _ready():
+	progress_bar.set_value(progress_bar.min_value)
+
+func _on_lantern_fuel_level_changed(fuel_level):
+	progress_bar.value = fuel_level
