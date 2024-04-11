72781ef689322bf412e95a074fa4fb779addffcb
theoschweizer <schweizer.theodore@gmail.com>
2024-04-11 15:48:13 -0500

    tutorial changes

--- interaction/text_box.gd
+++ interaction/text_box.gd
@@ -2,10 +2,15 @@ extends MarginContainer
 
 """
 """
+@export var text_to_display: String
 
 @onready var container: MarginContainer = $MarginContainer
 
 var label: Label = null
+
+func _ready():
+	if text_to_display:
+		display_text(text_to_display)
 
 func display_text(text_to_display: String):
 	for child in container.get_children():
