ad98da93b065c2cd164dcf64b4fe175b999ad7d7
Desmond Roberts <desmondr1@aol.com>
2024-04-22 09:35:27 -0500

    small bug fixes

--- objects/enemies/face.gd
+++ objects/enemies/face.gd
@@ -1,6 +1,5 @@
 extends Enemy
 
-
 """
 Mario-style falling war face that crushes the protagonist. No weak area, 
 cannot die.
@@ -13,17 +12,7 @@ var move_speed: int = 150
 func _ready():
 	super._ready()
 	anim.play("idle")
-	
-	# Initial hitbox setup
-	$Enemy/DamageArea/Frame0Hitbox.set_disabled(false)
-	$Enemy/DamageArea/Frame1Hitbox.set_disabled(true)
-	$Enemy/DamageArea/Frame2Hitbox.set_disabled(true)
-	$Enemy/DamageArea/Frame3Hitbox.set_disabled(true)
-	$Enemy/DamageArea/Frame4Hitbox.set_disabled(true)
-	$Enemy/DamageArea/Frame5to6Hitbox.set_disabled(true)
-	
-	$Enemy.velocity = Vector2(0, 50)
-
+	velocity = Vector2(0, 50)
 	for hitbox in damage_back.get_children():
 		hitbox.set_disabled(true)
 	for hitbox in damage_front.get_children():
@@ -42,7 +31,7 @@ func death_process():
 		$DequeueTimer.start()
 		#if abs(Global.protagonist.global_position.x - global_position.x) < 700:
 			#$Enemy/splat.play()
-		$Enemy/AnimationPlayer.play("splat")
+		$AnimationPlayer.play("splat")
 		dying = true
 
 func _on_animation_player_animation_finished(anim_name):
--- objects/reflecting_water.gd
+++ objects/reflecting_water.gd
@@ -11,7 +11,7 @@ Pool with shader ripple effect that reflects the protagonist.
 @onready var reflection_threshold_y = $ReflectionThreshold.global_position.y
 
 var protagonist
-const height = 250
+const height = 70
 @onready var reflection_start = surface_y + (surface_y - reflection_threshold_y)
 
 var free_head_reflections = []
@@ -31,8 +31,13 @@ func _physics_process(delta):
 	for head_reflection in used_head_reflections:
 		if head_reflection.global_position.y <= surface_y + 100:
 			head_reflection.play("splat")
+			#head_reflection.animation_finished.connect(stop_spaz)
 		else:
 			head_reflection.position.y -= 50 * delta
+			
+
+#func stop_spaz():
+	#head_reflection.play("splat_end")
 
 func _on_area_2d_body_entered(body):
 	if body is Protagonist:
@@ -45,6 +50,7 @@ func _on_area_2d_body_entered(body):
 		face_reflection.set_global_position(Vector2(body.global_position.x, global_position.y + height))
 		face_reflection.set_visible(true)
 		face_reflection.play("default")
+		face_reflection.set_scale(body.scale)
 		used_head_reflections.append(face_reflection)
 		body.tree_exiting.connect(destroy_reflection)
 
