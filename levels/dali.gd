3b1bcfb9da248553041c0d8f2024d7747980e337
Jessica Opsahl-Ong <jessica.opsahl.ong@gmail.com>
2024-04-29 16:11:40 -0500

    dali changes

--- levels/dali.gd
+++ levels/dali.gd
@@ -10,13 +10,17 @@ var enemy_speed = {"skin_blob": 100, "elephant": 100}
 @onready var anim = $AnimationPlayer
 @onready var protagonist_dali = preload("res://characters/protagonist_dali.tscn")
 
+var skin_blob = preload("res://objects/enemies/skin_blob.tscn")
+
 var duplicatedNode
-var throw_velocity = 250
+var throw_velocity = 350
+var speed_change = 0
 
 func _ready():
 	super._ready()
 	protagonist.fade_to_black.connect(Callable(self, "fade_to_black"))
 	protagonist.took_damage.connect(Callable(self, "protag_took_damage"))
+	protagonist.set_throw_velocity(throw_velocity)
 	
 	for clock in clocks.get_children():
 		clock.speed_up.connect(speed_up)
@@ -26,6 +30,7 @@ func _ready():
 func speed_up():
 	var valid = protagonist.take_damage(-1, true)
 	if valid:
+		speed_change += 20
 		throw_velocity += 25
 		protagonist.set_throw_velocity(throw_velocity)
 		if timer.wait_time > 0.125:
@@ -39,6 +44,7 @@ func speed_up():
 func slow_down():
 	var valid = protagonist.take_damage(1, true)
 	if valid:
+		speed_change -= 20
 		throw_velocity -= 25
 		protagonist.set_throw_velocity(throw_velocity)
 		timer.wait_time *= 2
@@ -82,10 +88,46 @@ func respawn():
 		duplicatedNode.position.y = checkpoint.position.y
 		get_tree().current_scene.add_child(duplicatedNode)
 		duplicatedNode.fade_to_black.connect(Callable(self, "fade_to_black"))
+		duplicatedNode.took_damage.connect(Callable(self, "protag_took_damage"))
 		duplicatedNode.set_throw_velocity(throw_velocity)
 		Global.protagonist = duplicatedNode
 		protagonist = duplicatedNode
+		
+		## RESPAWN SKIN BLOBS 
+		var new_skin_blob
+		if $Enemies/Path2D21.get_child_count() == 0:
+			new_skin_blob = skin_blob.instantiate()
+			new_skin_blob.move_speed = 120 + speed_change
+			new_skin_blob.scale.x = 0.2
+			new_skin_blob.scale.y = 0.2
+			$Enemies/Path2D21.add_child(new_skin_blob)
+		if $Enemies/Path2D20.get_child_count() == 0:
+			new_skin_blob = skin_blob.instantiate()
+			new_skin_blob.move_speed = 140 + speed_change
+			new_skin_blob.scale.x = 0.15
+			new_skin_blob.scale.y = 0.15
+			$Enemies/Path2D20.add_child(new_skin_blob)
+		if $Enemies/Path2D15.get_child_count() == 0:
+			new_skin_blob = skin_blob.instantiate()
+			new_skin_blob.move_speed = 110 + speed_change
+			new_skin_blob.scale.x = 0.25
+			new_skin_blob.scale.y = 0.25
+			$Enemies/Path2D15.add_child(new_skin_blob)
+		if $Enemies/Path2D16.get_child_count() == 0:
+			new_skin_blob = skin_blob.instantiate()
+			new_skin_blob.move_speed = 140 + speed_change
+			new_skin_blob.scale.x = 0.15
+			new_skin_blob.scale.y = 0.15
+			$Enemies/Path2D16.add_child(new_skin_blob)
+		if $Enemies/Path2D17.get_child_count() == 0:
+			new_skin_blob = skin_blob.instantiate()
+			new_skin_blob.move_speed = 70 + speed_change
+			new_skin_blob.scale.x = 0.3
+			new_skin_blob.scale.y = 0.3
+			$Enemies/Path2D17.add_child(new_skin_blob)
+		
 		anim.play("scene_in")
+		print("protagonist health: ", protagonist.health)
 		protag_took_damage()
 	else:
 		get_tree().reload_current_scene()
